68d2508abae8d38adc71f0ce971091247aba6bca
Alex Benishek <alex@alexbenishek.com>
2018-11-17 10:54:13 -0500

    Probably more bugs, and needs an "Occurs" check in unifier, but works on simple test cases

--- main.lisp
+++ main.lisp
@@ -5,4 +5,13 @@
 
 (in-package :main)
 
-(defun main () nil)
+(defun main ()
+  (let* (
+	 (a (make-instance 'schema:fol-variable :name "a"))
+	 (b (make-instance 'schema:fol-variable :name "b"))
+	 (c (make-instance 'schema:fol-function :name "c"))
+	 (d (make-instance 'schema:fol-function :name "d"))
+	 (y (make-instance 'schema:fol-predicate :name "P" :args (list a c)))
+	 (x (make-instance 'schema:fol-predicate :name "P" :args (list d b)))
+	 (z (schema:most-general-unifier y x)))
+    (print-object z *standard-output*)))
--- schema.lisp
+++ schema.lisp
@@ -3,14 +3,18 @@
   (:use :common-lisp
         :alexandria
         :trivia)
-  (:export most-general-unifier))
+  (:export most-general-unifier
+	   fol-predicate
+	   fol-variable
+	   fol-function))
 
 (in-package :schema)
 (declaim (optimize (speed 0) (space 0) (safety 3) (debug 3)))
 
 (defgeneric fol-eq (a b)
   (:documentation "Equality as specified for first order logic formulas."))
-(defmethod fol-eq ((a standard-object) (b standard-object)))
+(defmethod fol-eq ((a t) (b t))
+  nil)
 
 (defclass fol-formula (standard-object)
   ()
@@ -30,6 +34,9 @@
     :documentation "The name of the variable as a string")))
 (defmethod fol-eq ((a fol-value) (b fol-value))
   (equal (slot-value a 'name) (slot-value b 'name)))
+(defmethod print-object ((object fol-variable) out)
+  (format out "V.~a" (slot-value object 'name)))
+
 (defclass fol-function (fol-value)
   ((name
     :initarg :name
@@ -48,7 +55,9 @@
                                             (slot-value a 'args)
                                             (slot-value b 'args))
            :initial-value t)))
-
+(defmethod print-object ((object fol-function) out)
+  (with-slots (name args) object
+    (format out "F.~a(~{~a~^,~})" name args)))
 (defclass fol-predicate (fol-statement)
   ((name
     :initarg :name
@@ -67,14 +76,17 @@
                                             (slot-value a 'args)
                                             (slot-value b 'args))
            :initial-value t)))
-
+(defmethod print-object ((object fol-predicate) out)
+  (with-slots (name args) object
+    (format out "P.~a(~{~a~^,~})" name args)))
 (defstruct unifier
   "A table for replacing wff by other wff"
   (renames (make-hash-table :test #'equalp) :type hash-table))
 
 (defun add-sym-rename (u k v)
-  (setf (gethash k (unifier-renames u)) (find-sym-rename u v))
-  v)
+  (let ((rename (find-sym-rename u v)))
+    (setf (gethash k (unifier-renames u)) rename)
+    rename))
 (defun find-sym-rename (u name)
   (let ((rename (gethash name (unifier-renames u))))
     (if rename
@@ -99,13 +111,11 @@
          (set-name (n r)
            (add-sym-rename unifier n r)
            )
-         (get-name (n)
-           (find-sym-rename unifier n))
          (replace-lit-in (lit wff)
            (if (fol-eq lit wff)
                lit
                (match wff
-                      ((class fol-variable (name n))
+                      ((class fol-variable)
                        lit)
                       ((class fol-function (name n) (args a))
                        (make-instance 'fol-function
@@ -122,19 +132,23 @@
 	 (unify-values (v w)
            (let ((new-name
                   (match v
-                    ((class fol-variable (name x))
-                     (set-name x w))
+                    ((class fol-variable)
+                     (set-name v w))
                     ((class fol-function (name n) (args x))
                      (match w
                        ((class fol-function (name m) (args y))
                         (if (equalp n m)
                             (make-instance 'fol-function :name n :args (mapcar #'unify-values x y))
-                            (error "Could not unify functions with different names")))))
+                            (error (format nil "Could not unify functions ~a, ~a, as they have different names.~%" v w))))
+		       ((class fol-variable)
+			(make-instance 'fol-function :name n :args x))
+		       (_
+			(error (format nil "Could not unify function ~a with ~a, which is neither a variable nor function.~%" v w)))))
                     (_
                      (match w
-                       ((class fol-variable (name y))
-                        (set-name w y))
-                       (_ (error "Could not unify variables")))))))
+                       ((class fol-variable)
+                        (set-name w v))
+                       (_ (error (format nil "Could not unify two unknown objects, ~a, ~a" v w))))))))
              (setf a (replace-lit-in new-name a))
              (setf b (replace-lit-in new-name b))
              new-name)))
